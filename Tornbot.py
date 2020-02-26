5e00d391e940eeb795b2e07baa3de0e4d2a43c65
Rubixel <harrisoncom3@gmail.com>
2020-02-25 21:49:34 -0500

    Added bloodbags channel stuff for event
    added that to help

--- Tornbot.py
+++ Tornbot.py
@@ -4,10 +4,11 @@ import json
 import requests
 import time
 import datetime
+import asyncio
 
-# botinfo
+# bot info
 client = discord.Client()
-# apikey
+# api key
 apiKey = 'SmKfloSmgSDgHZ6e'
 
 # Stuff to prevent API key being used >100 times.
@@ -16,7 +17,10 @@ startTime = datetime.datetime.now()
 apiLimit = 85
 apiChecks = 0
 lastMinute = True
-
+bloodBagChannel = False
+temp_armory_time_stamp = False
+armory_time_stamp = 0
+shutdown = False
 
 # Limit API use function
 def apichecklimit():
@@ -44,12 +48,55 @@ def apichecklimit():
         time.sleep(60)
 
 
+async def check_bags():
+    global armory_time_stamp
+    global bloodBagChannel
+    temp_armory_time_stamp = 0
+    r = requests.get("https://api.torn.com/faction/?selections=armorynews&key=SmKfloSmgSDgHZ6e")
+    armory_logs = json.loads(r.text)["armorynews"]
+    bagFound = False
+    if bloodBagChannel == False:
+        return
+    for log_ID in armory_logs:
+        log = armory_logs[log_ID]
+        log_timestamp = int(log["timestamp"])
+        print("saved TS" + str(armory_time_stamp))
+        print('log TS' + str(log_timestamp))
+        if log_timestamp <= armory_time_stamp:
+            if bagFound is True:
+                armory_time_stamp = temp_armory_time_stamp
+            return
+        if log_timestamp > armory_time_stamp:
+            if temp_armory_time_stamp < log_timestamp:
+                print("temp updated")
+                temp_armory_time_stamp = log_timestamp
+        if log["news"].find("filled one of the faction's Empty Blood Bag items.") != -1:
+            bagFound = True
+            string = log["news"]
+            playerid = string[string.find("XID=")+4:string.find("\">")]
+            playername = string[string.find("\">")+2:string.find("</a>")]
+            await bloodBagChannel.send(playername + " ["+playerid+"] filled one of the faction's Empty Blood Bag ""items.")
+            print(log_timestamp)
+            print(playername + " ["+playerid+"] filled one of the faction's Empty Blood Bag ""items.")
+    if bagFound is True:
+        armory_time_stamp = temp_armory_time_stamp
+    print("End Saved TS" + str(armory_time_stamp))
+
+async def wait_minute():
+    print("waiting")
+    if shutdown is True:
+        print("Blood bag command shutdown")
+        await bloodBagChannel.send("Blood Bags shutdown")
+        return
+    await asyncio.sleep(60)
+    await check_bags()
+    await wait_minute()
+
 @client.event
-# onbotload
+# on bot load
 async def on_ready():
     print("Program is ready.")
     await client.change_presence(activity=discord.Game('Bullying Everyone!'))
-
 
 @client.event
 # whenever a message is sent
@@ -90,23 +137,23 @@ async def on_message(message):
         members = parsedJSON["members"]
         onliners = []
         await message.channel.send('Please wait, generating list.')
-        for tornid in members:
+        for torn_id in members:
             # checks every members and adds to table
-            rID = requests.get('https://api.torn.com/user/' + tornid + '?selections=profile&key=%s' % apiKey)
+            rID = requests.get('https://api.torn.com/user/' + torn_id + '?selections=profile&key=%s' % apiKey)
             apichecklimit()
-            idrequest = json.loads(rID.text)
-            if json.dumps(idrequest)[0:8] == '{"error"':
-                await message.channel.send(json.dumps(idrequest))
+            id_request = json.loads(rID.text)
+            if json.dumps(id_request)[0:8] == '{"error"':
+                await message.channel.send(json.dumps(id_request))
                 return
-            lastaction = idrequest['last_action']['relative']
-            playername = idrequest['name']
-            splitstrings = (lastaction.split())
-            if splitstrings[1] == "minutes" and int(splitstrings[0]) < 6:
-                onliners.append(playername + " [" + tornid + '] ' + lastaction)
-        sendstring = "Players online in the past 5 minutes: \n ```"
+            last_action = id_request['last_action']['relative']
+            player_name = id_request['name']
+            split_strings = (last_action.split())
+            if split_strings[1] == "minutes" and int(split_strings[0]) < 6:
+                onliners.append(player_name + " [" + torn_id + '] ' + last_action)
+        send_string = "Players online in the past 5 minutes: \n ```"
         for state in onliners:
-            sendstring = (sendstring + " " + state + " " + "\n")
-        await message.channel.send(sendstring + '```')
+            send_string = (send_string + " " + state + " " + "\n")
+        await message.channel.send(send_string + '```')
         # prints faction inactives
     elif message.content[0:10] == '!inactives':
         factionid = ""
@@ -125,21 +172,22 @@ async def on_message(message):
         inactives = []
         members = parsedJSON["members"]
         await message.channel.send('Please wait, generating list.')
-        for tornid in members:
+        for torn_id in members:
             apichecklimit()
-            data = json.loads(requests.get('https://api.torn.com/user/' + tornid + '?selections=profile&key=%s' % apiKey
+            data = json.loads(requests.get('https://api.torn.com/user/' + torn_id + '?selections='
+                                                                                    'profile&key=%s' % apiKey
                                            ).text)
             actiondata = data['last_action']['relative']
-            playername = data['name']
-            splitstrings = actiondata.split()
-            if splitstrings[1] == "hours" and int(splitstrings[0]) > 10:
-                inactives.append(playername + " [" + tornid + '] ' + actiondata)
-            elif splitstrings[1] == "day" or splitstrings[1] == "days":
-                inactives.append(playername + " [" + tornid + '] ' + actiondata)
-        sendstring = "Players Inactive for 4 hours or more: \n ```"
+            player_name = data['name']
+            split_strings = actiondata.split()
+            if split_strings[1] == "hours" and int(split_strings[0]) > 10:
+                inactives.append(player_name + " [" + torn_id + '] ' + actiondata)
+            elif split_strings[1] == "day" or split_strings[1] == "days":
+                inactives.append(player_name + " [" + torn_id + '] ' + actiondata)
+        send_string = "Players Inactive for 4 hours or more: \n ```"
         for state in inactives:
-            sendstring = (sendstring + " " + state + " " + "\n")
-        await message.channel.send(sendstring + "```")
+            send_string = (send_string + " " + state + " " + "\n")
+        await message.channel.send(send_string + "```")
     elif message.content[0:9] == "!donators":
         factionid = ""
         if message.content[10:len(message.content)]:
@@ -157,33 +205,35 @@ async def on_message(message):
         await message.channel.send('Please wait, generating list.')
         members = parsedJSON["members"]
         donators = []
-        for tornid in members:
+        for torn_id in members:
             donator = False
             property = False
             apichecklimit()
-            data = json.loads(requests.get('https://api.torn.com/user/' + tornid + '?selections=profile&key=%s' %
+            data = json.loads(requests.get('https://api.torn.com/user/' + torn_id + '?selections=profile&key=%s' %
                                            apiKey).text)
             playerName = data["name"]
-            propstring = ""
-            donatestring = ""
+            prop_string = ""
+            donate_string = ""
             if data["donator"] == 0:
                 donator = True
-                donatestring = " Donator - False"
+                donate_string = " Donator - False"
             if data["property"] != "Private Island":
                 property = True
-                propstring = (" Property - " + data["property"])
+                prop_string = (" Property - " + data["property"])
             if donator is True or property is True:
-                donators.append(playerName + ": " + donatestring + " " + propstring)
-        sendstring = "Players without Donator Status or PI: \n ```"
+                donators.append(playerName + ": " + donate_string + " " + prop_string)
+        send_string = "Players without Donator Status or PI: \n ```"
         for string in donators:
-            sendstring = (sendstring + " " + string + " " + "\n")
-        await message.channel.send(sendstring + "```")
+            send_string = (send_string + " " + string + " " + "\n")
+        await message.channel.send(send_string + "```")
     elif message.content == "!help":
         await message.channel.send(
             "```css\nHelp screen:\n\nFaction:\n\t!onliners [faction_ID] -> Prints players active in the last five "
             "minutes.\n\t!inactives [faction_ID] -> Prints players inactive for over 10 hours.\n\t!donator "
             "[faction_ID]  -> Prints players without donator status, and or a PI.\n\nPlayer:\n\t!torn [player_ID] -> "
-            "Prints a player's basic information & status.\n\nMisc:\n\t!help -> Prints help screen.```")
+            "Prints a player's basic information & status.\n\nMisc:\n\t!help -> Prints help screen.\n\t!bindbags -> "
+            "Posts all bloodbag filling info in this channel.(Do not use twice, or it will break)"
+            "```")
     elif message.content[0:7] == "!verify":
         verifyID = message.content[8:len(message.content)]
         tornname = json.loads(
@@ -210,6 +260,16 @@ async def on_message(message):
         elif discordID == str(message.author.id):
             await message.channel.send("Welcome " + tornname + " [" + verifyID + "]!")
             await message.author.edit(nick=tornname + " [" + verifyID + "]")
+    elif message.content == "!bindbags":
+        global bloodBagChannel
+        bloodBagChannel = client.get_channel(message.channel.id)
+        await bloodBagChannel.send("Bloodbag filling will now output to: #" + message.channel.name+"!")
+        await wait_minute()
+        await check_bags()
+    elif message.content == "!shutdown":
+        global shutdown
+        shutdown = True
+
 
 
 client.run('NTc4Mzk0MzIwNTMzNzE3MDIy.XNy-TA.qNJMsPDOraaATcoBYb-ZxivYn94')
