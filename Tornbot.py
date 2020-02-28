ee7a8c648fd836e0f635ee01207a92cd340aef51
Rubixel <harrisoncom3@gmail.com>
2020-02-28 00:23:37 -0500

    Added random_bully to bully random people

--- Tornbot.py
+++ Tornbot.py
@@ -5,6 +5,7 @@ import requests
 import time
 import datetime
 import asyncio
+import random
 
 # bot info
 client = discord.Client()
@@ -13,22 +14,39 @@ apiKey = 'SmKfloSmgSDgHZ6e'
 
 # Stuff to prevent API key being used >100 times.
 
+
+# todo print version number/time etc information for debugging
+# todo also create stuff to help debugging in nohup.out
+
+
 startTime = datetime.datetime.now()
 apiLimit = 85
 apiChecks = 0
 lastMinute = True
-bloodBagChannel = False
-npcChannel = False
-temp_armory_time_stamp = False
 armory_time_stamp = 0
 shutdown = False
 leslie_ready = False
 duke_ready = False
-#testing mode, for testing only one thing so it doesnt interfere with running bot
+# testing mode, for testing only one thing so it doesnt interfere with running bot
 testing_mode = True
-council_plus = ["ns leaders","ns council i","ns council ii", "ns council iii", "ns ii leaders"]
+council_plus = ["ns leaders", "ns council i", "ns council ii", "ns council iii", "ns ii leaders"]
+admin_users = [200676892024504320, 547926928313548801, 547986194055692289]
 bloodBagChannel = ""
 npcChannel = ""
+# todo assign timer() variables on the first call instead of having them in global
+npcTime = 0
+bagTime = 0
+printTime = 0
+randBully = 0
+debug = False
+bully_list = ["Hcom", "jukka", "Gratify", "ZeroTwo", "STEVE_HOLT", "Rhino", "LTJELLOMAN", "Bones", "Roxy", "Everyone"]
+
+def printd(*args):
+    if debug:
+        print(args)
+
+
+# todo Comment all of the code before making repository public.
 
 # Limit API use function
 def apichecklimit():
@@ -63,7 +81,7 @@ async def check_bags():
     r = requests.get('https://api.torn.com/faction/?selections=armorynews&key=%s' % apiKey)
     armory_logs = json.loads(r.text)["armorynews"]
     bagFound = False
-    if bloodBagChannel == False:
+    if bloodBagChannel is False:
         return
     for log_ID in armory_logs:
         log = armory_logs[log_ID]
@@ -80,10 +98,12 @@ async def check_bags():
             string = log["news"]
             playerid = string[string.find("XID=")+4:string.find("\">")]
             playername = string[string.find("\">")+2:string.find("</a>")]
-            await bloodBagChannel.send(playername + " ["+playerid+"] filled one of the faction's Empty Blood Bag ""items.")
+            await bloodBagChannel.send(playername + " ["+playerid+"] filled one of the faction's "
+                                                                  "Empty Blood Bag ""items.")
     if bagFound is True:
         armory_time_stamp = temp_armory_time_stamp
 
+# todo add in bit of formatting for leslie & duke message: NPC_NAME will be at level four in etc etc...
 async def check_leslie():
     global npcChannel
     global leslie_ready
@@ -96,11 +116,13 @@ async def check_leslie():
         return
     if four_time < 400 and leslie_ready is False:
         leslie_ready = True
-        ready_minutes = str(four_time //60)
-        ready_seconds = str(four_time %60)
+        ready_minutes = str(four_time // 60)
+        ready_seconds = str(four_time % 60)
         await npcChannel.send("Leslie will be ready in: "+ready_minutes+" minutes and "+ready_seconds+" "
                               "seconds! <@&612556617153511435>\n"+"https://www.torn.com/loader2.php?sid=get"
                                                                   "InAttack&user2ID=15")
+        
+        
 async def check_duke():
     global npcChannel
     global duke_ready
@@ -113,55 +135,75 @@ async def check_duke():
         return
     if four_time < 400 and duke_ready is False:
         duke_ready = True
-        ready_minutes = str(four_time //60)
-        ready_seconds = str(four_time %60)
+        ready_minutes = str(four_time // 60)
+        ready_seconds = str(four_time % 60)
         await npcChannel.send("Duke will be ready in: "+ready_minutes+" minutes and "+ready_seconds+" "
                               "seconds! <@&612556617153511435>\n"+"https://www.torn.com/loader2.php?sid=get"
                                                                   "InAttack&user2ID=4")
 
-def checkCouncilRoles(roleList):
+
+def check_council_roles(role_list):
     global council_plus
-    for authorRole in roleList:
+    for authorRole in role_list:
         for councilRole in council_plus:
             if authorRole.name.lower() == councilRole:
                 return True
     return False
 
-async def npc_clock():
-    await asyncio.sleep(60)
-    await check_duke()
-    await check_leslie()
-    await npc_clock()
 
-async def wait_minute():
-    if shutdown is True:
-        print("Blood bag command shutdown")
-        await bloodBagChannel.send("Blood Bags shutdown")
-        return
-    await asyncio.sleep(60)
-    await check_bags()
-    await wait_minute()
+async def timer():
+    global bagTime
+    global npcTime
+    global printTime
+    global randBully
+    global bully_list
+    bagTime += 20
+    npcTime += 20
+    printTime += 20
+    randBully += 20
+    if bagTime >= 60:
+        bagTime = 0
+        await check_bags()
+    if npcTime >= 100:
+        npcTime = 0
+        await check_duke()
+        await check_leslie()
+    if printTime >= 1800:
+        printTime = 0
+        print("================================")
+        print("Bi-hourly time:")
+        print("Start time: " + str(time.time()))
+        print("Start time: " + str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))))
+    if randBully >= 3600:
+        random.seed()
+        rand = random.randrange(0, len(bully_list))
+        await client.change_presence(activity=discord.Game('Bullying ' + bully_list[rand] + "!"))
+    await asyncio.sleep(20)
+    await timer()
+
 
 @client.event
 # on bot load
 async def on_ready():
+    printd("on ready")
     global armory_time_stamp
-    await client.change_presence(activity=discord.Game('Bullying Everyone!'))
-   # if testing_mode == True:
-    #    return
+    global bully_list
+    random.seed()
+    rand = random.randrange(0, len(bully_list))
+    await client.change_presence(activity=discord.Game('Bullying ' + bully_list[rand]+"!"))
     global bloodBagChannel
     bloodBagChannel = client.get_channel(645540955688271872)
     r = requests.get('https://api.torn.com/torn/?selections=timestamp&key=%s' % apiKey)
     armory_time_stamp = json.loads(r.text)["timestamp"]
-   # print("Connected to: " + bloodBagChannel.name + " " + str(armory_time_stamp))
     global npcChannel
     npcChannel = client.get_channel(586185860505010176)
-    print("Connected to: " + npcChannel.name)
-   # await npcChannel.send("NPC information will now output to: #" + message.channel.name + "!")
-    await wait_minute()
-    await check_bags()
-    await npc_clock()
-    print("Program is ready.")
+    print("================================")
+    print("Start time: "+str(time.time()))
+    print("Start time: "+str(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))))
+    print("Tornbot.py is ready.")
+    print("================================")
+    await timer()
+
 
 @client.event
 # whenever a message is sent
@@ -171,10 +213,10 @@ async def on_message(message):
         return
     # checks a player for info
     if message.content[0:5] == "!torn":
-        if checkCouncilRoles(message.author.roles) is False:
+        if check_council_roles(message.author.roles) is False:
             await message.author.send("You do not have permissions to use this command: \"" + message.content + "\"")
             return
-        if testing_mode == True:
+        if testing_mode is True:
             return
         idtocheck = message.content[6:len(message.content)]
         if idtocheck == "":
@@ -191,10 +233,10 @@ async def on_message(message):
             info['status']['description'] + '.')
     # all faction onliners last 5 mins
     elif message.content[0:9] == '!onliners':
-        if checkCouncilRoles(message.author.roles) is False:
+        if check_council_roles(message.author.roles) is False:
             await message.author.send("You do not have permissions to use this command: \""+message.content+"\"")
             return
-        if testing_mode == True:
+        if testing_mode is True:
             return
         factionid = ""
         if message.content[9:len(message.content)]:
@@ -225,10 +267,10 @@ async def on_message(message):
         await message.channel.send(send_string + '```')
         # prints faction inactives
     elif message.content[0:10] == '!inactives':
-        if checkCouncilRoles(message.author.roles) is False:
+        if check_council_roles(message.author.roles) is False:
             await message.author.send("You do not have permissions to use this command: \""+message.content+"\"")
             return
-        if testing_mode == True:
+        if testing_mode is True:
             return
         factionid = ""
         if message.content[10:len(message.content)]:
@@ -260,10 +302,10 @@ async def on_message(message):
             send_string = (send_string + " " + state + " " + "\n")
         await message.channel.send(send_string + "```")
     elif message.content[0:9] == "!donators":
-        if checkCouncilRoles(message.author.roles) is False:
+        if check_council_roles(message.author.roles) is False:
             await message.author.send("You do not have permissions to use this command: \""+message.content+"\"")
             return
-        if testing_mode == True:
+        if testing_mode is True:
             return
         factionid = ""
         if message.content[10:len(message.content)]:
@@ -303,9 +345,8 @@ async def on_message(message):
             send_string = (send_string + " " + string + " " + "\n")
         await message.channel.send(send_string + "```")
     elif message.content == "!help":
-        #todo update todo to use embed format
-        #todo add who can use commands
-        if testing_mode == True:
+        # todo update todo to use embed format
+        if testing_mode is True:
             return
         await message.channel.send(
             "```css\nHelp screen:\n\nFaction:\n\t!onliners [faction_ID] -> Prints players active in the last five "
@@ -315,42 +356,47 @@ async def on_message(message):
             "Posts all bloodbag filling info in this channel.ᴿ(Do not use twice, or it will break)\n\t!bindNPC -> "
             "Prints NPC updates int his channel.ᴿ\n\t\tᴿIs restricted to council & leaders."
             "```")
-    #elif message.content[0:7] == "!verify":
-       # verifyID = message.content[8:len(message.content)]
-       # tornname = json.loads(
-        #    requests.get(('https://api.torn.com/user/' + verifyID + '?selections=basic&key=%s' % apiKey)).text)[
-        #    "name"]
-      #  data = json.loads(requests.get('https://api.torn.com/user/' + verifyID + '?selections=discord&key=%s' % apiKey
-      #                                 ).text)
-       # apichecklimit()
-     #   if '{"error": {"code": 6, "error": "Incorrect ID"}}' == json.dumps(data):
-        #    await message.channel.send("Error(Code6): Incorrect ID")
-        #    return
-       # discordID = data["discord"]["discordID"]
-     #   if discordID == "":
-       #     await message.channel.send(
-           #     tornname + " [" + verifyID + "] is not associated with a discord account. Please verify in Torn's "
-                        #                     "Discord server: https://discordapp.com/invite/TVstvww" + "<@" + str(
-                       #                       message.author.id) + ">")
-      #  elif discordID != str(message.author.id):
-         #   await message.channel.send(
-           #     tornname + " [" + verifyID + "] is associated with another discod account. Please verify with your "
-                 #                            "Discord account in Torn's Discord server: https://discordapp.com/invite/"
-                  #                           "TVstvww " + "<@" + str(
-             #                                 message.author.id) + ">")
-     #   elif discordID == str(message.author.id):
-      #      await message.channel.send("Welcome " + tornname + " [" + verifyID + "]!")
-       #     await message.author.edit(nick=tornname + " [" + verifyID + "]")
+    elif message.content[0:7] == "!verifyBullyBot":
+        verifyID = message.content[8:len(message.content)]
+        tornname = json.loads(
+            requests.get(('https://api.torn.com/user/' + verifyID + '?selections=basic&key=%s' % apiKey)).text)[
+            "name"]
+        data = json.loads(requests.get('https://api.torn.com/user/' + verifyID + '?selections=discord&key=%s' % apiKey
+                                       ).text)
+        apichecklimit()
+        if '{"error": {"code": 6, "error": "Incorrect ID"}}' == json.dumps(data):
+            await message.channel.send("Error(Code6): Incorrect ID")
+            return
+        discordID = data["discord"]["discordID"]
+        if discordID == "":
+            await message.channel.send(
+                tornname + " [" + verifyID + "] is not associated with a discord account. Please verify in Torn's "
+                                            "Discord server: https://discordapp.com/invite/TVstvww" + "<@" + str(
+                                           message.author.id) + ">")
+        elif discordID != str(message.author.id):
+            await message.channel.send(
+                tornname + " [" + verifyID + "] is associated with another discod account. Please verify with your "
+                                             "Discord account in Torn's Discord server: https://discordapp.com/invite/"
+                                             "TVstvww " + "<@" + str(
+                                              message.author.id) + ">")
+        elif discordID == str(message.author.id):
+            await message.channel.send("Welcome " + tornname + " [" + verifyID + "]!")
+            await message.author.edit(nick=tornname + " [" + verifyID + "]")
     elif message.content == "!shutdown":
-        if checkCouncilRoles(message.author.roles) is False:
+        if check_council_roles(message.author.roles) is False:
             await message.author.send("You do not have permissions to use this command: \""+message.content+"\"")
             return
-        if testing_mode == True:
+        if testing_mode is True:
             return
         global shutdown
         shutdown = True
     elif message.content == "!getchannelinfo":
+        if check_council_roles(message.author.roles) is False:
+            await message.author.send("You do not have permissions to use this command: \"" + message.content + "\"")
         print(message.channel.id)
+        print(message.author.id)
+
+
 client.run('NTc4Mzk0MzIwNTMzNzE3MDIy.XNy-TA.qNJMsPDOraaATcoBYb-ZxivYn94')
 
 
